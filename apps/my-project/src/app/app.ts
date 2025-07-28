710506e58251721d972bd0ea424193a955f9573c
Johnna Schulz - Desktop <jschulz_d@lucastree.com>
2025-07-28 10:21:54 -0400

    add hero component

--- apps/my-project/src/app/app.ts
+++ apps/my-project/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcome } from './nx-welcome';
+import { Hero } from '@my-project/ui';
 
 @Component({
-  imports: [NxWelcome, RouterModule],
+  imports: [NxWelcome, RouterModule, Hero],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
