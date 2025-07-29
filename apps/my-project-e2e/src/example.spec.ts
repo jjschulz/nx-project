267fc4a606abfe9cc50bb2088f48b3c8f233899f
jjschulz <johnnajanelle01@gmail.com>
2025-07-29 09:01:21 -0400

    push to main

--- apps/my-project-e2e/src/example.spec.ts
+++ apps/my-project-e2e/src/example.spec.ts
@@ -1,8 +1,30 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('/');
+test.describe('navigation', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/');
+  });
 
+  test('has title', async ({ page }) => {
   // Expect h1 to contain a substring.
-  expect(await page.locator('h1').innerText()).toContain('Welcome');
-});
+    expect(await page.locator('h1').innerText()).toContain('Welcome');
+  });
+
+  test('has hero component', async ({ page }) => {
+    const hero = page.locator('lib-hero');
+    await expect(hero).toBeVisible();
+    
+    // await expect(hero.locator('.title')).toHaveText('Welcome my-project');
+    // await expect(hero.locator('.subtitle')).toHaveText('Build something amazing today');
+    // await expect(hero.locator('.cta')).toHaveText('Get Started');
+  });
+
+  test('screenshots', async ({ page }) => {
+    await expect(page).toHaveScreenshot();
+  });
+
+
+})
+
+
+
