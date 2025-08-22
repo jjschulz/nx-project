3ac02da7e313a8facfcfbfa2036ce2082aad09c1
jjschulz <johnnajanelle01@gmail.com>
2025-08-22 14:59:26 -0400

    add arrow onclick, and text input

--- apps/my-project/src/app/app.ts
+++ apps/my-project/src/app/app.ts
@@ -2,6 +2,7 @@ import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcome } from './nx-welcome';
 import { Hero } from '@my-project/ui';
+import { TouchedChangeEvent } from '@angular/forms';
 
 @Component({
   imports: [NxWelcome, RouterModule, Hero],
@@ -16,6 +17,7 @@ export class App implements AfterViewInit{
   private ctx!: CanvasRenderingContext2D;
   private pushArray: string[] = [];
   private step=-1;
+  private mode="";
   constructor() {}
 
   ngAfterViewInit(): void {
@@ -26,6 +28,7 @@ export class App implements AfterViewInit{
       const file=(e.target as HTMLInputElement).files?.[0];
       if(!file) return;
       this.pushArray.length=0;
+      this.step=-1;
       const img = new Image();
       img.onload=()=>{
         this.ctx.clearRect(0,0,canvas.width,canvas.height);
@@ -35,14 +38,35 @@ export class App implements AfterViewInit{
       img.src=URL.createObjectURL(file);
       return img;
     });
-  
-    canvas.addEventListener('mousedown', (e) => this.startDraw(e, canvas));
-    canvas.addEventListener('mousemove', (e)=>this.draw(e, 3, canvas));
-    canvas.addEventListener('mouseup', ()=>this.stopDraw(canvas));
-    canvas.addEventListener('mouseleave', ()=>this.stopDraw(canvas));
-    canvas.addEventListener('touchstart', (e) => this.startDraw(e, canvas));
-    canvas.addEventListener('touchmove', (e)=>this.draw(e, 3, canvas));
-    canvas.addEventListener('touchend', ()=>this.stopDraw(canvas));
+    
+    const freedrawButton=document.getElementById('freedraw');
+    freedrawButton?.addEventListener('click', ()=>{
+      console.log('mode:', this.mode);
+      canvas.addEventListener('mousedown', (e) => this.startDraw(e, canvas));
+      canvas.addEventListener('mousemove', (e)=>this.draw(e, 3, canvas));
+      canvas.addEventListener('mouseup', ()=>this.stopDraw(canvas));
+      canvas.addEventListener('mouseleave', ()=>this.stopDraw(canvas));
+      canvas.addEventListener('touchstart', (e) => this.startDraw(e, canvas));
+      canvas.addEventListener('touchmove', (e)=>this.draw(e, 3, canvas));
+      canvas.addEventListener('touchend', ()=>this.stopDraw(canvas));
+    });
+    ////////////////////////
+
+    const arrowButton=document.getElementById('arrow');
+    arrowButton?.addEventListener('click',(e)=>{
+      this.addArrow(this.ctx,e);
+    });
+
+
+    ///////////////////////
+
+    const textButton=document.getElementById('text');
+    textButton?.addEventListener('click',()=>{
+        this.addText(this.ctx);
+    });
+
+
+    ///////////////////////
 
     const saveButton=document.getElementById('saveButton');
     saveButton?.addEventListener('click', ()=>{
@@ -56,8 +80,6 @@ export class App implements AfterViewInit{
     redoButton?.addEventListener('click', ()=>{
       this.redo(this.ctx);
     });
-
-
   }
 
   startDraw(e: MouseEvent | TouchEvent, canvas: any){
@@ -114,6 +136,10 @@ export class App implements AfterViewInit{
 
   }
 
+  saveAsOverlay(canvas:any){
+    //save just the drawings and not the image
+  }
+
   cpush(canvas:HTMLCanvasElement){
     this.step++;
     if (this.step < this.pushArray.length){
@@ -145,6 +171,68 @@ export class App implements AfterViewInit{
       canvasPic.src=this.pushArray[this.step];
       canvasPic.onload=function(){ctx.drawImage(canvasPic,0,0);}
     }
+  }
+
+  addArrow(ctx: CanvasRenderingContext2D, e:MouseEvent){
+    const img = new Image();
+    img.src = '../red-arrow-2.png';
+    img.onload = ()=> {
+        ctx.canvas.addEventListener('click', (event)=> {
+            const rect = ctx.canvas.getBoundingClientRect();
+            const x = event.clientX - rect.left;
+            const y = event.clientY - rect.top;
+
+            ctx.drawImage(img, x-50, y-50);
+            console.log('image drawn');
+            this.cpush(ctx.canvas)
+        });
+    };
+  }
+
+  addText(ctx: CanvasRenderingContext2D){
+    let currentText = "";
+    let textX = 0;
+    let textY = 0;
+    const hiddenInput = document.getElementById("hiddenInput") as HTMLInputElement;
+
+    ctx.canvas.addEventListener("click", function(event) {
+      textX = event.clientX - ctx.canvas.getBoundingClientRect().left;
+      textY = event.clientY - ctx.canvas.getBoundingClientRect().top;
+      hiddenInput!.style.left = textX + "px";
+      hiddenInput!.style.top = textY + "px";
+      hiddenInput!.style.width = "100px"; 
+      hiddenInput!.style.height = "20px"; 
+      hiddenInput!.style.opacity = '1'; 
+
+      hiddenInput!.focus();
+      hiddenInput!.value = ''; 
+    });
+
+    hiddenInput!.addEventListener("input", ()=> {
+      currentText = hiddenInput!.value;
+      this.ctx.font = "20px Arial";
+      this.ctx.fillStyle = "black";
+      this.ctx.fillText(currentText, textX, textY + 15); 
+    });
+
+    hiddenInput!.addEventListener("keydown", (event)=> {
+      if (event.key === "Enter") {
+        hiddenInput.style.opacity = '0';
+        hiddenInput!.blur(); 
+        this.cpush(ctx.canvas);
+      }
+    });
+  }
+
+  removeAllEventListeners(canvas: any){
+    canvas.removeEventListener('mousedown', (e: MouseEvent) => this.startDraw(e, canvas));
+    canvas.removeEventListener('mousemove', (e: MouseEvent)=>this.draw(e, 3, canvas));
+    canvas.removeEventListener('mouseup', ()=>this.stopDraw(canvas));
+    canvas.removeEventListener('mouseleave', ()=>this.stopDraw(canvas));
+    canvas.removeEventListener('touchstart', (e:TouchEvent) => this.startDraw(e, canvas));
+    canvas.removeEventListener('touchmove', (e:TouchEvent)=>this.draw(e, 3, canvas));
+    canvas.removeEventListener('touchend', ()=>this.stopDraw(canvas));
+
   }
 
 }
